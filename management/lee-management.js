3492f5399049f41e5a894675061fbb54ebd1fa84
Heather Booker <heather.booker@hotmail.com>
2015-11-27 18:54:56 -0500

    add management sector (cms for gallery imgs)

---
+++ management/lee-management.js
@@ -0,0 +1,44 @@
+var keyword;
+    function setKey(key) {
+        keyword = key;
+    }
+    $(".htmlGen").click(function(){
+        var newHtml = '<div class="element transition ' + keyword + '" data-category="transition"> <a href="assets-lee/img/' + fileName + '.jpg" class="thumb"><figure class="img-polaroid"><img src="assets-lee/img/' + fileName + '-crop.png"></figure></a></div>';
+        $(".htmlOutput").text(newHtml);
+    }); 
+    var fileName;
+    $('input[type="file"]').change(function(e){
+        fileName1 = e.target.files[0].name;
+        fileName = fileName1.split('.');
+        fileName.pop();
+    });
+
+window.onload = function() {
+        var options =
+        {
+            imageBox: '.imageBox',
+            thumbBox: '.thumbBox',
+            spinner: '.spinner',
+            imgSrc: 'avatar.png'
+        }
+        var cropper;
+        document.querySelector('#file').addEventListener('change', function(){
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                options.imgSrc = e.target.result;
+                cropper = new cropbox(options);
+            }
+            reader.readAsDataURL(this.files[0]);
+            this.files = [];
+        })
+        document.querySelector('#btnCrop').addEventListener('click', function(){
+            var img = cropper.getDataURL();
+            document.querySelector('.cropped').innerHTML += '<a href="'+img+'" download="'+ fileName +'-crop.png"> <img src="'+img+'"></a>';
+        })
+        document.querySelector('#btnZoomIn').addEventListener('click', function(){
+            cropper.zoomIn();
+        })
+        document.querySelector('#btnZoomOut').addEventListener('click', function(){
+            cropper.zoomOut();
+        })
+    };
